13d823417b98682a655839bda226d17761dfe039
Welden <welden@hotmail.sg>
2025-08-24 12:50:41 +0800

    Remove README.md and refactor task management in script.js and style.css

--- script.js
+++ script.js
@@ -20,42 +20,40 @@ const displayTasks = () => {
     tasksDiv.style.display = "none";
   }
 
-  //Clear the tasks
+  // Clear the tasks
   tasksDiv.innerHTML = "";
 
-  //Fetch All The Keys in local storage
+  // Fetch all the keys in local storage
   let tasks = Object.keys(localStorage);
   tasks = tasks.sort();
 
   for (let key of tasks) {
-    let classValue = "";
-
-    //Get all values
     let value = localStorage.getItem(key);
     let taskInnerDiv = document.createElement("div");
     taskInnerDiv.classList.add("task");
     taskInnerDiv.setAttribute("id", key);
     taskInnerDiv.innerHTML = `<span id="taskname">${key.split("_")[1]}</span>`;
-    //localstorage would store boolean as string so we parse it to boolean back
-    let editButton = document.createElement("button");
-    editButton.classList.add("edit");
-    editButton.innerHTML = `<button class="delete"><i class="fa-solid fa-trash"></i></button>`;
-    if (!JSON.parse(value)) {
-      editButton.style.visibility = "visible";
-    } else {
-      editButton.style.visibility = "hidden";
-      taskInnerDiv.classList.add("completed");
-    }
-    editButton.addEventListener("click", deleteTask);
-    taskInnerDiv.appendChild(editButton); 
+
+    // DELETE BUTTON
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete");
+    deleteButton.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+    deleteButton.addEventListener("click", (e) => {
+      e.stopPropagation();
+      const parent = e.target.closest(".task");
+      removeTask(parent.id);
+      parent.remove();
+      count -= 1;
+    });
+
+    taskInnerDiv.appendChild(deleteButton);
     tasksDiv.appendChild(taskInnerDiv);
   }
 
-  //tasks completed
+  // Mark tasks as completed on click
   tasks = document.querySelectorAll(".task");
   tasks.forEach((element, index) => {
     element.onclick = () => {
-      //local storage update
       if (element.classList.contains("completed")) {
         updateStorage(element.id.split("_")[0], element.innerText, false);
       } else {
@@ -63,38 +61,8 @@ const displayTasks = () => {
       }
     };
   });
-
-  //Edit Tasks
-  editTasks = document.getElementsByClassName("edit");
-  Array.from(editTasks).forEach((element, index) => {
-    element.addEventListener("click", (e) => {
-      //Stop propogation to outer elements (if removed when we click delete eventually rhw click will move to parent)
-      e.stopPropagation();
-      //disable other edit buttons when one task is being edited
-      disableButtons(true);
-      //update input value and remove div
-      let parent = element.parentElement;
-      newTaskInput.value = parent.querySelector("#taskname").innerText;
-      //set updateNote to the task that is being edited
-      updateNote = parent.id;
-      //remove task
-      parent.remove();
-    });
-  });
-
-  //Delete Tasks
-  deleteTasks = document.getElementsByClassName("delete");
-  Array.from(deleteTasks).forEach((element, index) => {
-    element.addEventListener("click", (e) => {
-      e.stopPropagation();
-      //Delete from local storage and remove div
-      let parent = element.parentElement;
-      removeTask(parent.id);
-      parent.remove();
-      count -= 1;
-    });
-  });
 };
+
 
 //Disable Edit Button
 const disableButtons = (bool) => {
@@ -119,13 +87,12 @@ const updateStorage = (index, taskValue, completed) => {
 
 // delete
 const deleteTask = (e) => {
-    e.stopPropagation();
-    const parent = e.target.parentElement.parentElement;
-    console.log(parent);
-    removeTask(parent.id);
-    parent.remove();
-    count -= 1;
-}
+  e.stopPropagation();
+  const parent = e.target.closest(".task");
+  removeTask(parent.id);
+  parent.remove();
+  count -= 1;
+};
 
 //Function To Add New Task
 document.querySelector("#push").addEventListener("click", () => {
@@ -148,5 +115,5 @@ document.querySelector("#push").addEventListener("click", () => {
     count += 1;
     newTaskInput.value = "";
   }
-  
+
 });
